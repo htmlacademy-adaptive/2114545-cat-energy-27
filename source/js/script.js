81983b891f8d603df6647085d91e9e4c47ae6da5
Ekaterina Zelentsova <kotocookie@yandex.ru>
2023-01-23 08:42:40 +0300

    before-after

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 let navMain = document.querySelector('.main-navigation');
 let navToggle = document.querySelector('.main-navigation__toggle');
+let slider = document.querySelector(".slider")
 
 navMain.classList.remove('main-navigation--nojs');
 navMain.classList.add('main-navigation--closed');
@@ -7,3 +8,31 @@ navMain.classList.add('main-navigation--closed');
 navToggle.addEventListener('click', function () {
 	navMain.classList.toggle('main-navigation--closed');
 });
+
+if (slider) {
+	let curtain = slider.querySelector(".slider__curtain")
+	let sliderStyles = getComputedStyle(slider)
+	let curtainPlaceStart
+	let clientX
+
+	function handleGrabCurtain (event) {
+		curtainPlaceStart = +(sliderStyles.getPropertyValue("--curtain-place"))
+		clientX = event.clientX
+		window.addEventListener("pointermove", handleMoveCurtain)
+	}
+
+	function handleMoveCurtain (event) {
+		let deltaX = event.clientX - clientX
+		let cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth
+		let curtainPlace = Math.min(Math.max(cursorPlace, 0), 1)
+		slider.style.setProperty("--curtain-place", `${curtainPlace}`)
+	}
+
+	function handleReleaseCurtaine () {
+		window.removeEventListener("pointermove", handleMoveCurtain)
+	}
+
+	window.addEventListener("pointerup", handleReleaseCurtaine)
+
+	curtain.addEventListener("pointerdown", handleGrabCurtain)
+}
